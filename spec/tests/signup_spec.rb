9e72c7d6504fcdb69d252466ea56bffd8b87a964
JithilSajanCoupa <jithil.sajan@coupa.com>
2022-01-10 00:13:34 +0530

    Login functionlity added

--- spec/tests/signup_spec.rb
+++ spec/tests/signup_spec.rb
@@ -7,6 +7,9 @@ feature "automation practise test" , type: :feature do
         homepage.signin_link.click
         loginpage= AUTOMATIONPRACTICE::Loginpage.new
         loginpage.load
+        loginpage.email_input.set "joey@gmail.com"
+        loginpage.password_input.set "password"
+        loginpage.login_btn.click
         binding.pry
     end
 
